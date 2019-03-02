ca4d1c885e709164748d0a1474421d15a94230bd
Gary Bermudez <garybm.17@gmail.com>
2019-03-01 16:16:02 -0800

    add extern fonts, and install special font loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -38,6 +39,17 @@ module.exports = {
           }
         ]
       },
+
+          {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+    },
 
       {
         test:/\.html$/,
